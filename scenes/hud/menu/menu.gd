cf5b4c78d7aa66590004075b64d995af01e780ec
artur <artur.nobre.114@ufrn.edu.br>
2026-04-09 16:49:41 -0300

    feat: código de menu melhorado

--- scenes/hud/menu/menu.gd
+++ scenes/hud/menu/menu.gd
@@ -1,37 +1,24 @@
 class_name Menu
 extends Control
 
-@onready var gameover: Label = $PanelContainer/gameover
+@onready var textlabel: Label = $PanelContainer/textlabel
 @onready var buttoncontainer: VBoxContainer = $PanelContainer/buttoncontainer
 @onready var nextlevel: Button = $PanelContainer/buttoncontainer/nextlevel
 @onready var restart: Button = $PanelContainer/buttoncontainer/restart
 @onready var menu: Button = $PanelContainer/buttoncontainer/menu
 @onready var quit: Button = $PanelContainer/buttoncontainer/quit
+const gameover: String = "GAME OVER!"
+const congratulation: String = "CONGRATULATIONS!"
 
+func handle_menu(option: String) -> void:
+	match option:
+		"next": pass
+		"restart": pass
+		"mainmenu": pass
+		"quit": get_tree().quit()
 
+func setgameovertext() -> void:
+	textlabel.text = gameover
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_nextlevel_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_restart_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_menu_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_quit_pressed() -> void:
-	pass # Replace with function body.
+func setwintext() -> void:
+	textlabel.text = congratulation
