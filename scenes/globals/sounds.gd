855cb5ec159811f522d14cebe662648c075bda56
glaucopaiva13 <glaucopaiva013@gmail.com>
2026-05-14 03:16:46 -0300

    feat: sound effects

--- scenes/globals/sounds.gd
+++ scenes/globals/sounds.gd
@@ -10,27 +10,35 @@ extends Node
 # NOTE: Não se preocupe com a chamada desses métodos
 
 @onready var music: AudioStreamPlayer = $Music;
+@onready var spawn: AudioStreamPlayer = $Spawn;
+@onready var base_damage: AudioStreamPlayer = $"Base Damage"
+@onready var swords_collision: AudioStreamPlayer = $"Swords collision";
+@onready var throw_arrow: AudioStreamPlayer = $"Throw arrow";
+@onready var arrow_collision: AudioStreamPlayer = $"Arrow collision";
+@onready var magic_ball_collision: AudioStreamPlayer = $"Magic ball collision";
+@onready var throw_magic_ball: AudioStreamPlayer = $"Throw magic ball";
+
 
 func play_music() -> void:
 	self.music.play();
 
 func play_base_damage_sound() -> void:
-	pass;
+	self.base_damage.play();
 
 func play_swords_collision_sound() -> void:
-	pass;
+	self.swords_collision.play();
 
 func play_throw_arrow_sound() -> void:
-	pass;
+	self.throw_arrow.play();
 
 func play_arrow_collision_sound() -> void:
-	pass;
+	self.arrow_collision.play();
 
 func play_throw_magic_ball_sound() -> void:
-	pass;
+	self.throw_magic_ball.play();
 
 func play_magic_ball_collision_sound() -> void:
-	pass;
+	self.magic_ball_collision.play();
 	
 func play_spawn_sound() -> void:
-	pass;
+	self.spawn.play();
